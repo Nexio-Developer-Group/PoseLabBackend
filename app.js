e6e08c96d5f03218a339fb9df30cd374da5b1ca3
Aftab Khan <dev.aftabkhan@gmail.com>
2025-12-24 00:43:33 +0530

    google and otp login

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const authRoutes = require("./routes/auth");
 require("./firebase"); // initialize firebase
 
 const app = express();
+
+app.use(cors({
+  origin: [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500"
+  ],
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
 app.use("/api", authRoutes); 
