98f25a2cd7fb62f41deb8e8c716a498b7fa341bd
Aftab Khan <dev.aftabkhan@gmail.com>
2025-12-26 00:00:23 +0530

    update cors origin

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "http://localhost:5500",
+    "http://localhost:5173",
     "http://127.0.0.1:5500",
     "https://poselab-backend.nexiotech.cloud"
   ],
